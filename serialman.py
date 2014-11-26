c2ebbb139a98644827136bf7cf4afa9ab17605e7
Philipp Klaus <philipp.klaus@gmail.com>
2014-11-26 13:17:41 +0100

    Using small timeout for read() instead of sleeping.

--- serialman.py
+++ serialman.py
@@ -20,14 +20,14 @@ class SerialManager(Process):
         settings['bytesize'] = serial.EIGHTBITS
         settings['parity'] = serial.PARITY_NONE
         settings['stopbits'] = serial.STOPBITS_ONE
-        settings['timeout'] = 0
+        settings['timeout'] = 0.0005
         settings.update(kwargs)
         self._kwargs = settings
         self.ser = serial.Serial(device, **self._kwargs)
         self.in_queue = Queue()
         self.out_queue = Queue()
         self.closing = False # A flag to indicate thread shutdown
-        self.sleeptime = 0.0005
+        self.sleeptime = None
         self._chunker = None
         Process.__init__(self, target=self.loop)
 
@@ -38,7 +38,7 @@ class SerialManager(Process):
     def loop(self):
         try:
             while not self.closing:
-                time.sleep(self.sleeptime)
+                if self.sleeptime: time.sleep(self.sleeptime)
                 in_data = self.ser.read(256)
                 if in_data:
                     if self._chunker:
@@ -60,12 +60,13 @@ class SerialManager(Process):
 def main():
     import argparse
     parser = argparse.ArgumentParser(description='A class to manage reading and writing from and to a serial port.')
-    parser.add_argument('--sleeptime', '-s', type=float, default=0.001, help='Seconds to sleep between polling (0.001s).')
+    parser.add_argument('--timeout', '-t', type=float, default=0.0005, help='Seconds until reading from serial port times out [default: 0.0005].')
+    parser.add_argument('--sleeptime', '-s', type=float, default=None, help='Seconds to sleep before reading from serial port again [default: none].')
     parser.add_argument('--baudrate', '-b', type=int, default=9600, help='Baudrate of serial port.')
     parser.add_argument('device', help='The serial port to use (COM4, /dev/ttyUSB1 or similar).')
     args = parser.parse_args()
 
-    s1 = SerialManager(args.device, baudrate=args.baudrate)
+    s1 = SerialManager(args.device, baudrate=args.baudrate, timeout=args.timeout)
     s1.sleeptime = args.sleeptime
     s1.start()
 
